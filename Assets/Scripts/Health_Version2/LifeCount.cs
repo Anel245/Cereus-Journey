156fc2115bc90f191692298a0998747f7e0ab33e
Anel245 <94172427+Anel245@users.noreply.github.com>
2021-11-30 22:38:58 +0100

    Save3

--- Assets/Scripts/Health_Version2/LifeCount.cs
+++ Assets/Scripts/Health_Version2/LifeCount.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeCount : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image[] lives;
+    public int livesRemaining;
+
+    //4 lives - 4 images (0,1,2,3)
+    //3 lives - 3 images (0,1,2,[3])
+    //2 lives - 2 images (0,1,[2],[3])
+    //1 life - 1 image (0,[1],[2],[3])
+    //0 lives - 0 images ([0],[1],[2],[3]) LOSE
+
+    public void LoseLife()
     {
-        
+        //Decrease the value of livesRemaining
+        livesRemaining--;
+        //Hide one of the life image
+        lives[livesRemaining].enabled = false;
+
+     
+        // If we run out lives we lose the game
+        if(livesRemaining==0)
+        {
+            PlayerManager.isGameOver = true;
+            gameObject.SetActive(false);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+            LoseLife();
     }
 }
