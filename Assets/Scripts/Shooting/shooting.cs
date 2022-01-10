5c9db5c431b33528889409c18e6cb750f06abb59
Anel245 <94172427+Anel245@users.noreply.github.com>
2022-01-10 16:14:12 +0100

    Shooting Save 1
    For me

---
+++ Assets/Scripts/Shooting/shooting.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firePoint;
+    public float bulletSpeed = 50;
+
+    Vector2 lookDirection;
+    float lookAngle;
+
+    void Update()
+    {
+        lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+        //firePoint.Rotate(new Vector3(lookDirection.x, lookDirection.y, 0f));
+        //   = Quaternion.Euler(0, 0, lookAngle);
+        //firePoint.rotation.SetLookRotation(lookDirection);
+        //firePoint.rotation.SetLookRotation(new Vector3(lookDirection.x, lookDirection.y, 0f));
+        firePoint.Rotate(new Vector3(0f, 0f, 1f));
+        Debug.Log("lookAngle: " + lookAngle + ", lookDirection: " + lookDirection);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletClone = Instantiate(bullet);
+            bulletClone.transform.position = firePoint.position;
+            bulletClone.transform.rotation = Quaternion.Euler(0, 0, lookAngle);
+
+            bulletClone.GetComponent<Rigidbody2D>().velocity = firePoint.right * bulletSpeed;
+        }
+    }
+}
+
