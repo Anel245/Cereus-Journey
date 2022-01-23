8d9ffc3de50a7cb673941e293a85997b57f6e184
Anel245 <94172427+Anel245@users.noreply.github.com>
2022-01-23 18:33:14 +0100

    Basic Menu template

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+    public void GoToSettingsMenu()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void GoToControlsMenu()
+    {
+        SceneManager.LoadScene("ControlsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
