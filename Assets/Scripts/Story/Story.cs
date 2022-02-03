604ccac536a3a56a674e414cc5aadb790dc06cc4
Anel245 <94172427+Anel245@users.noreply.github.com>
2022-02-03 20:18:48 +0100

    Prefabs

--- Assets/Scripts/Story/Story.cs
+++ Assets/Scripts/Story/Story.cs
@@ -19,30 +19,13 @@ public class Story : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("StoryDetection");
-        if (collision.IsTouchingLayers(10, 11, 12, 13) == "Story_1")       
+        if (collision.gameObject.layer == 10)       
         {
             Debug.Log("Story_1");
             SoundManager.PlaySound("Triggerbox1");
             Destroy(collision.gameObject);
         }
-        if (collision.IsTouchingLayers = "Story_2")
-        {
-            Debug.Log("Story_1");
-            SoundManager.PlaySound("Triggerbox2");
-            Destroy(collision.gameObject);
-        }
-        if (collision.IsTouchingLayers = "Story_3")
-        {
-            Debug.Log("Story_1");
-            SoundManager.PlaySound("Triggerbox3");
-            Destroy(collision.gameObject);
-        }
-        if (collision.IsTouchingLayers = "Story_4")
-        {
-            Debug.Log("Story_1");
-            SoundManager.PlaySound("Triggerbox4");
-            Destroy(collision.gameObject);
-        }
+
 
     }
 }
